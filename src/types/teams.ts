40da65267deb8efb9d43118379579284e37094d1
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2025-09-18 00:52:02 -0300

    adiciona interface Teams e atualiza escudos dos times no JSON

---
+++ src/types/teams.ts
@@ -0,0 +1,16 @@
+export interface Teams {
+  id: number
+  nome: string
+  apelidos: string[]
+  fundacao: string
+  cidade: string
+  estadio: string
+  capacidade_estadio: number
+  titulos_estaduais: number
+  titulos_nacionais: any[]
+  cor_principal: string[]
+  mascote: string
+  site_oficial: string
+  escudo: string
+  categoria: string
+}
