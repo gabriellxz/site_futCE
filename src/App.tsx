11894cfc753bf4c5310e6ea2033d93ec1938f546
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2025-09-18 00:51:05 -0300

    adiciona rota para a página de times

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home/Home'
+import AllTeams from './Pages/AllTeams/AllTeams'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/times" element={<AllTeams/>} />
       </Routes>
     </BrowserRouter>
   )
