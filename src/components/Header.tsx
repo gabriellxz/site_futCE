8e4af43c8b1410553c7c6b0d7a6f5e593a58f88f
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2025-09-19 14:01:30 -0300

    remove ícone de menu e ajusta visibilidade dos links no cabeçalho

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Header() {
@@ -6,15 +5,16 @@ export default function Header() {
         <header className="flex justify-between items-center p-3 fixed w-full bg-white z-20">
             <Link to={"/"} className="text-xl font-bold italic">FutCE</Link>
 
-            <div className="hidden sm:flex gap-4">
+            <div className="flex gap-4">
                 <Link to="/times">Times</Link>
                 <Link to="/notícias">Notícias</Link>
                 <Link to="/idols">Ídolos</Link>
+                <Link to="/estádios">Estádios</Link>
             </div>
 
-            <div className="sm:hidden">
+            {/* <div className="sm:hidden">
                 <FaBars />
-            </div>
+            </div> */}
         </header>
     )
 }
